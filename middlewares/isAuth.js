5aa996545e6c045f5daf43b276d40822103e31a9
bda <badrudozaansari0786@gmail.com>
2026-05-17 18:10:36 +0530

    fixed cookie settings1

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -2,6 +2,8 @@ import "dotenv/config";
 import jwt from "jsonwebtoken";
 
 const isAuth = async (req, res, next) => {
+    console.log(req.cookies);
+    console.log(req.headers.cookie);
     try {
 
         const token = req.cookies?.token;
@@ -35,6 +37,5 @@ const isAuth = async (req, res, next) => {
         });
     }
 };
-console.log(req.cookies);
-console.log(req.headers.cookie);
+
 export default isAuth;
