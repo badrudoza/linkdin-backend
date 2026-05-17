306dda901692e7efe3196c635f4cce2dc96d481c
bda <badrudozaansari0786@gmail.com>
2026-05-17 17:39:23 +0530

    rool back

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,135 +1,80 @@
-import genToken from "../config/token.js";
-import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
-
+import genToken from "../config/token.js"
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
 export const signUp = async (req, res) => {
     try {
-
-        const {
-            firstName,
-            lastName,
-            userName,
-            email,
-            password
-        } = req.body;
-
-        let existEmail = await User.findOne({ email });
-
+        const { firstName, lastName, userName, email, password } = req.body
+        let existEmail = await User.findOne({ email })
         if (existEmail) {
-            return res.status(400).json({
-                message: "Email already exists!"
-            });
+            return res.status(400).json({ message: "email already exist !" })
         }
-
-        let existUsername = await User.findOne({ userName });
-
+        let existUsername = await User.findOne({ userName })
         if (existUsername) {
-            return res.status(400).json({
-                message: "Username already exists!"
-            });
+            return res.status(400).json({ message: "userName already exist !" })
         }
-
         if (password.length < 8) {
-            return res.status(400).json({
-                message: "Password must be at least 8 characters"
-            });
+            return res.status(400).json({ message: "password must be at least 8 characters" })
         }
 
-        let hashedPassword = await bcrypt.hash(password, 10);
+        let hassedPassword = await bcrypt.hash(password, 10)
 
         const user = await User.create({
             firstName,
             lastName,
             userName,
             email,
-            password: hashedPassword
-        });
+            password: hassedPassword
+        })
 
-        let token = await genToken(user._id);
-
+        let token = await genToken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: true,
-            sameSite: "none",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        });
-
-        return res.status(201).json(user);
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production"
+        })
+        return res.status(201).json(user)
 
     } catch (error) {
-
         console.log(error);
+        return res.status(500).json({ message: "signup error" })
 
-        return res.status(500).json({
-            message: "Signup error"
-        });
     }
-};
+}
 
 export const login = async (req, res) => {
     try {
-
-        const { email, password } = req.body;
-
-        let user = await User.findOne({ email });
-
+        const { email, password } = req.body
+        let user = await User.findOne({ email })
         if (!user) {
-            return res.status(400).json({
-                message: "User does not exist!"
-            });
+            return res.status(400).json({ message: "user does not exist !" })
         }
 
-        const isMatch = await bcrypt.compare(
-            password,
-            user.password
-        );
-
+        const isMatch = await bcrypt.compare(password, user.password)
         if (!isMatch) {
-            return res.status(400).json({
-                message: "Incorrect password"
-            });
+            return res.status(400).json({ message: "incorrect password" })
         }
 
-        let token = await genToken(user._id);
-
+        let token = await genToken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: true,
-            sameSite: "none",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        });
-
-        return res.status(200).json(user);
-
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV=== "production"
+        })
+        return res.status(200).json(user)
     } catch (error) {
-
         console.log(error);
-
-        return res.status(500).json({
-            message: "Login error"
-        });
+        return res.status(500).json({ message: "login error" })
     }
-};
+}
 
 export const logOut = async (req, res) => {
     try {
-
-        res.clearCookie("token", {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none"
-        });
-
-        return res.status(200).json({
-            message: "Logout successfully"
-        });
-
+        res.clearCookie("token")
+        return res.status(200).json({ message: "log out successfully" })
     } catch (error) {
-
         console.log(error);
-
-        return res.status(500).json({
-            message: "Logout error"
-        });
+        return res.status(500).json({ message: "logout error" })
     }
-};
+}
